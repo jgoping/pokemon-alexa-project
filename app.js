71322f34bc4ee2516cdca1e50530e676975756d7
jgoping <32006038+jgoping@users.noreply.github.com>
2020-09-29 10:31:35 -0400

    Setup ExpressAdapter from ask-sdk-express-adapter

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const { ExpressAdapter } = require('ask-sdk-express-adapter');
 const app = express();
+const Alexa = require('ask-sdk-core');
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+const skill = skillBuilder.create();
+const adapter = new ExpressAdapter(skill, false, false);
 
 app.post('/', adapter.getRequestHandlers());
 
