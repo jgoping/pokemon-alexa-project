2f0efa454b4dbd3511e07dfdbe421c5709945ce7
jgoping <32006038+jgoping@users.noreply.github.com>
2020-09-29 10:33:50 -0400

    Create LaunchRequestHandler

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const app = express();
 const Alexa = require('ask-sdk-core');
 const skillBuilder = Alexa.SkillBuilders.custom();
 
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const say = 'Welcome to the Pokemon Info skill! What would you like to know?';
+
+        return handlerInput.responseBuilder
+            .speak(say)
+            .reprompt(say)
+            .getResponse();
+    }
+};
+
+skillBuilder.addRequestHandlers(
+    LaunchRequestHandler,
+);
+
 const skill = skillBuilder.create();
 const adapter = new ExpressAdapter(skill, false, false);
 
